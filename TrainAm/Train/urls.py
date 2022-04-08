c4e14f876223d947d38a49428d8795579d0c3c61
Nghi Tran <nghitrn135@yahoo.com>
2022-04-08 16:24:07 +0200

    django

---
+++ TrainAm/Train/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path("accounts/", include("django.contrib.auth.urls"), name="login"),
+]
