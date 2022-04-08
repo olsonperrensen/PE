4f521d2e285e42d1c6fbab17d6230cda0b2fe8fc
Maximiliano Iturria <r0834721@student.thomasmore.be>
2022-04-08 13:01:58 +0200

    Better table

--- file__obtention.py
+++ file__obtention.py
@@ -3,30 +3,14 @@
 
 from bs4 import BeautifulSoup
 from url import *
-import urllib.error, urllib.parse, urllib.request, sys, ast
+import urllib.error, urllib.parse, urllib.request, sys, ast, certifi
 
 # Handles
 go = open('go.txt','wb')
-come = open('come.txt','wb')
-web = urllib.request.urlopen(getURL()) # Own fn
+web = urllib.request.urlopen("https://k.vnticketonline.vn/#/thongtinhanhtrinh/result/SGO/HNO/2022-04-28/76725", cafile=certifi.where()) # Own fn
 soup = BeautifulSoup(web, 'html.parser')
 l = list()
 
 # Convert to file
-go_t = soup.find(attrs = {'id':'ctl00_ContentPlaceHolderMain_GridView1'}).get_text().encode().strip()
-come_t = soup.find(attrs = {'id':'ctl00_ContentPlaceHolderMain_GridView2'}).get_text().encode().strip()
-go.write(go_t)
-come.write(come_t)
-go.close()
-come.close()
-
-# Polish
-go = open('go.txt','rb')
-come = open('come.txt','rb')
-
-for line in go:
-    line = line.decode().strip()
-    if line == '': continue
-    l.append(line)
-
-print(l)
+go_t = soup()
+print(go_t)
--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
+import json
 
 with open('./table.html', encoding='utf-8') as f:
   content = f.read()
   soup = BeautifulSoup(content, 'html.parser')
   row = soup.select('tr > td > div > span')
-  print(row)
+  counter = 1
+  for i in row:
+    print(i.get_text())
+    counter+=1
+    if(counter%6==0):
+      print("\n")
