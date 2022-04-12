ee8dca9c9b03cdb782bfde92393a73b7da459d08
Nghi Tran <nghitrn135@yahoo.com>
2022-04-12 12:19:19 +0200

    change test.py

--- test.py
+++ test.py
@@ -3,22 +3,12 @@ import json
 
 with open('./table.html', encoding='utf-8') as f:
   content = f.read()
-  table ={
-    'Departure': [],
-    'Distance(km)': [],
-    'Date': [],
-    'Time': [],
-    'Departure Time': []
-  }
+  table =[]
   soup = BeautifulSoup(content, 'html.parser')
   row = soup.select('tr')
   for i in row[1:]:
     col = i.select('span')
-    table['Departure'].append(col[0].get_text())
-    table['Distance(km)'].append(col[1].get_text())
-    table['Date'].append(col[2].get_text())
-    table['Time'].append(col[3].get_text())
-    table['Departure Time'].append(col[4].get_text())
+    table.append({'Departure':col[0].get_text(), 'Distance(km)':col[1].get_text(), 'Date': col[2].get_text(), 'Time':col[3].get_text(), 'Departure Time':col[4].get_text()})
   with open('table.json', 'w') as data_json:
     json.dump(table, data_json)
   with open('table.json') as test:
