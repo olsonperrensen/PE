382ef49a554a41b19cbec547f15002c82cf4790b
Maximiliano Iturria <r0834721@student.thomasmore.be>
2022-04-13 13:16:12 +0200

    Fixed bug in mapStation.py, now all cities get their code.

--- GPU/mapStation.py
+++ GPU/mapStation.py
@@ -6,13 +6,19 @@ data = open("LoadDmGa.xml",encoding="utf8").read()
 xml = ET.fromstring(data)
 bunch = xml.findall('./DMGa/SKeys')
 exact_keys = xml.findall('./DMGa/MaGa')
+exact_words = xml.findall('./DMGa/TenGa')
 
     
 
 def mapStation(user_station):
-    for common_words, exact_key in zip(bunch,exact_keys):
+    for common_words, exact_key, exact_word in zip(bunch,exact_keys,exact_words):
+        EXACT_CODE = exact_key.text
+        EXACT_WORD = exact_word.text
         if common_words.text is not None:
-            tmp_list = common_words.text.split(',')
+            tmp_list = common_words.text.split(', ')
             for word in tmp_list:
                 if word == user_station:
-                    return exact_key.text
+                    return EXACT_CODE
+        elif common_words.text is None:
+            if EXACT_WORD == user_station:
+                return EXACT_WORD
