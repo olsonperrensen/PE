c989ef670f3cfe245d259a015bdf3e63be4b0217
Max <htcevo4gnougat@gmail.com>
2022-04-17 11:37:29 +0200

    Basic form validation for datepicker added

--- src/app/datepicker/datepicker.component.ts
+++ src/app/datepicker/datepicker.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import {Router } from '@angular/router';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { Journey } from '../table/journey';
@@ -9,43 +10,15 @@ import { Journey } from '../table/journey';
   styleUrls: ['./datepicker.component.css']
 })
 export class DatepickerComponent implements OnInit {
-  @ViewChild('fromInput',{static:true}) fromInput!:ElementRef;
-  @ViewChild('toInput',{static:true}) toInput!:ElementRef;
 
   constructor(private router:Router) { }
 
-  @ViewChild('toInput') to!:ElementRef;
-  @ViewChild('fromInput') from!:ElementRef;
-  @ViewChild('d') d!:ElementRef;
-
-  user_journey!:Journey;
 
   ngOnInit(): void {
   }
 
-  model!: NgbDateStruct;
-
-
-
-  submit()
+  onSubmit(f:NgForm)
   {
-    if(this.fromInput.nativeElement.value.length < 1 
-      ||
-      this.toInput.nativeElement.value.length < 1
-      ||
-      this.d.nativeElement.value.length < 1
-       )
-    {
-      this.router.navigate(['/error']);
-    }
-    else
-    {
-      this.user_journey = {
-        from: this.from.nativeElement.value,
-        to: this.to.nativeElement.value,
-        day: this.d.nativeElement.value
-      }
-      this.router.navigate(['/','table',this.user_journey.from,this.user_journey.to,this.user_journey.day]);
-    }
+    console.log(f)
   }
 }
