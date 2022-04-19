991ee45f75be741040d8bd9ff666cbb1690341d3
Max <htcevo4gnougat@gmail.com>
2022-04-19 20:31:15 +0200

    Mockup for log-in page.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  isLoggedIn:boolean=false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(f:NgForm)
+  {
+    console.log(f.value);
+    this.isLoggedIn=true;
+  }
 }
