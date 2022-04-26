476821922deebb28db0637c14d7ac4ef9446a7af
Max <htcevo4gnougat@gmail.com>
2022-04-26 17:55:57 +0200

    fix help-component pulling issue

---
+++ src/app/help-contact/help-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-help-contact',
+  templateUrl: './help-contact.component.html',
+  styleUrls: ['./help-contact.component.css']
+})
+export class HelpContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
