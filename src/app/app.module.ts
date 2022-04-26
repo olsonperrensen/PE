13c1b038a6cecb06f683a7e9f1a1eb2d210a157e
Max <htcevo4gnougat@gmail.com>
2022-04-26 22:42:55 +0200

    Added offline package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,8 @@ import { SitemapComponent } from './sitemap/sitemap.component';
 import { HelpContactComponent } from './help-contact/help-contact.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
@@ -75,6 +77,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     AppRoutingModule,
     BrowserAnimationsModule,
     FontAwesomeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [JourneyDetailsService,TableGuardServiceGuard],
   bootstrap: [AppComponent]
