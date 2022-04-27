9b50df95cf41776e8abd8afa19f88e28e90c25e6
Max <htcevo4gnougat@gmail.com>
2022-04-27 21:32:37 +0200

    White theme by default.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ComponentFactoryResolver, ElementRef, EventEmitter, OnChanges, OnInit, Output, Renderer2, SimpleChanges, ViewChild } from '@angular/core';
 import {DarkModeService} from 'angular-dark-mode';
 import { Observable } from 'rxjs';
 import { DarkModeStatusService } from '../dark-mode-status.service';
@@ -8,20 +8,33 @@ import { DarkModeStatusService } from '../dark-mode-status.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit{
 
-  @ViewChild('switch') switch:any;
+  @ViewChild('switch') switch!:HTMLInputElement;
+  switch_status!:boolean;
 
   darkMode$: Observable<boolean> = this.darkModeService.darkMode$;
  
-  constructor(private darkModeService: DarkModeService,private darkModeStatus:DarkModeStatusService) {}
+  constructor(private darkModeService: DarkModeService,
+    private darkModeStatus:DarkModeStatusService, private R2:Renderer2) {}
  
   onToggle(): void {
     this.darkModeService.toggle();
-    this.darkModeStatus.setStatus(this.switch.value);
+    this.darkModeStatus.setStatus(Boolean(this.switch.value));
   }
 
   ngOnInit(): void {
+    if(localStorage.getItem('dark-mode')==='{"darkMode":false}')
+    {
+      this.darkModeStatus.setStatus(false);
+      this.switch_status = false;
+    }
+    else
+    {
+      this.darkModeStatus.setStatus(true);
+      this.switch_status = true;
+    }
+    // this.darkModeService.toggle();
   }
 
 }
