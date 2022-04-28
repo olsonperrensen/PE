5f7fe2503097f5d377294f25247d750e2a0b1c2b
Max <htcevo4gnougat@gmail.com>
2022-04-28 16:18:55 +0200

    Footer animations

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as a from 'angular-animations';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  animations:[
+    a.tadaAnimation(),
+    a.heartBeatAnimation()
+  ]
 })
 export class FooterComponent implements OnInit {
   constructor() { }
-
+  isLinkMouseOver:boolean = false;
+  isGNUMouseOver:boolean = false;
+  onLinkMouseOver()
+  {
+    this.isLinkMouseOver = true;
+    setTimeout(() => {
+      this.isLinkMouseOver = false;  
+    }, 2800);
+  }
+  onGNUMouseOver()
+  {
+    this.isGNUMouseOver = true;
+    setTimeout(() => {
+      this.isGNUMouseOver = false;  
+    }, 2800);
+  }
   ngOnInit(): void {
   }
 
