13feb2acf6fdf4fd9701ad522888825e4bcae959
Max <htcevo4gnougat@gmail.com>
2022-04-28 23:58:30 +0200

    "What our customers are saying" added on /about-us page.

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -5,14 +5,28 @@ import *  as a from 'angular-animations';
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css'],
   animations:[
-    a.fadeInOnEnterAnimation()
+    a.fadeInOnEnterAnimation(),
+    a.slideInLeftOnEnterAnimation(),
+    a.slideOutRightAnimation()
   ]
 })
-export class AboutUsComponent implements OnInit {
+export class AboutUsComponent implements OnInit{
+
+  first_visible:boolean=true;
+  anim_out:boolean=false;
 
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.first_visible = !this.first_visible;
+    }, 5000);
+    if(this.first_visible)
+    {
+      setInterval(() => {
+        this.anim_out = !this.anim_out;
+      }, 4000);
+    }
   }
 
 }
