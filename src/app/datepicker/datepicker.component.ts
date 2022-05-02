b5e086373e27666c055d72937fa8a92df7750f08
Max <htcevo4gnougat@gmail.com>
2022-05-02 12:59:56 +0200

    google maps now with dummy markers (will soon be train stations)

--- src/app/datepicker/datepicker.component.ts
+++ src/app/datepicker/datepicker.component.ts
@@ -69,6 +69,50 @@ export class DatepickerComponent implements OnInit, OnDestroy, AfterViewInit {
   lat = 10.8231;
   lng = 106.6297;
   privateKey = 'AIzaSyCsTw56lFc40e_ObgyNVmQOQCung5JGL8w';
+  markers = [
+    {
+        "id": "1",
+        "lat": "10.782522909849995",
+        "lng": "106.67749116477299",
+        "name": "Bogot\u00e1"
+    }, 
+    {
+        "id": "2",
+        "lat": "6.24478548",
+        "lng": "-75.57050110",
+        "name": "Medell\u00edn"
+    }, 
+    {
+        "id": "3",
+        "lat": "7.06125013",
+        "lng": "-73.84928550",
+        "name": "Barrancabermeja"
+    }, 
+    {
+        "id": "4",
+        "lat": "7.88475514",
+        "lng": "-72.49432589",
+        "name": "C\u00facuta"
+    }, 
+    {
+        "id": "5",
+        "lat": "3.48835279",
+        "lng": "-76.51532198",
+        "name": "Cali"
+    }, 
+    {
+        "id": "6",
+        "lat": "4.13510880",
+        "lng": "-73.63690401",
+        "name": "Villavicencio"
+    }, 
+    {
+        "id": "7",
+        "lat": "6.55526689",
+        "lng": "-73.13373892",
+        "name": "San Gil"
+    }
+];
 
   // Register html form
   @ViewChild('f') f!: NgForm;
