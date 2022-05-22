f86ad1b72ae907169f493c140d96cd5128659b4e
olsonperrensen <80784184+olsonperrensen@users.noreply.github.com>
2022-05-22 18:48:29 +0000

    Delete TrainAm directory

--- TrainAm/Train/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path("accounts/", include("django.contrib.auth.urls"), name="login"),
-]
