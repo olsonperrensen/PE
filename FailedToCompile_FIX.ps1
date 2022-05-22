6d24e163ab5c59a368c7c152dcbb8e7e376172d3
Max <htcevo4gnougat@gmail.com>
2022-05-22 20:49:55 +0200

    PowerShell to fix 'failed to compile'

---
+++ FailedToCompile_FIX.ps1
@@ -0,0 +1,7 @@
+﻿$configFiles = gci BackgroundMask.d.ts, IBackgroundMask.d.ts, CanvasUtils.d.ts -Recurse
+foreach ($file in $configFiles)
+{
+    (Get-Content $file.PSPath) |
+    Foreach-Object { $_ -replace "GlobalCompositeOperation", "any" } |
+    Set-Content $file.PSPath
+}
