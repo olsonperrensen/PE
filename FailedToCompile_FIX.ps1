7e5d9c3ce5b656eace8398c5b83707eb1bd112b0
Max <htcevo4gnougat@gmail.com>
2022-05-22 21:55:32 +0200

    removed fix for new fix

--- FailedToCompile_FIX.ps1
+++
@@ -1,7 +0,0 @@
-﻿$configFiles = gci BackgroundMask.d.ts, IBackgroundMask.d.ts, CanvasUtils.d.ts -Recurse
-foreach ($file in $configFiles)
-{
-    (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace "GlobalCompositeOperation", "any" } |
-    Set-Content $file.PSPath
-}
